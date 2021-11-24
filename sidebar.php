56d693e9eb44c1e5ec18d5914da7d6db66c90008
Ian Hiew <pixdo.et@gmail.com>
2021-11-24 11:27:06 +0800

    first test for left sidebar

---
+++ sidebar.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+include("includes/config.inc.php");
+?>
+<?php
+if(isset($_SESSION['loggedin']) && isset($_SESSION['userid']))
+{
+?>
+<table class="dashedtable" width="100%" cellspacing="0" cellpadding="2">
+    <tbody>
+        <tr>
+            <td style="text-align:right;">
+                <p>
+                </p>
+                <form method="post" action="login.php">
+                    Email:<br> <input type="text" class="inputtext" name="email" value="" size="20"><br> Password:<br>
+                    <input type="password" class="inputtext" name="pass" size="20"><br>
+                    <div style="float:center">
+                        <input type="submit" class="inputsubmit" value="login">&nbsp;
+                        <input type="button" class="inputsubmit" value="register" onclick="javascript:document.location='register.php';">
+                    </div>
+                </form>
+                <!--<br>-->
+            </td>
+        </tr>
+    </tbody>
+</table>
+<?php
+}
+else
+{
+?>
+<table class="dashedtable" width="100%" cellspacing="0" cellpadding="2">
+    <tbody>
+        <tr>
+            <td style="text-align:right">
+                <p><a href="profile.php?id=<?php echo $_SESSION['userid'];?>">My Profile</a><a href="edit_profile.php?id=<?php echo $_SESSION['id'];?>"> [ Edit ] </a></p>
+                <p><a href=".#">My friends</a></p>
+                <p><a href=".#">My groups</a></p>
+                <p><a href=".#">My parties</a></p>
+                <p><a href=".#">My messages</a></p>
+                <p><a href="edit_profile.php?id=<?php echo $_SESSION['userid'];?>">My account</a></p>
+                <p><a href=".#">My privacy</a></p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<?php
+}
+?>
--- sidebar1.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-?>
-<table class="dashedtable" width="100%" cellspacing="0" cellpadding="2">
-              <tbody><tr><td align="right">
-  <p style="text-align:Left;"><a style="text-align:Left" href="profile.php?id=2">My profile</a><span style="text-align:Right"><?php if (isset($_SESSION['id']){echo "<a href='edit_profile.php?id=".$_GET['id']."'> [ Edit ]</a></span>";}else{echo "";}?>
-                  </p>
-                  <!--<br>-->
-              </td></tr></tbody></table>
